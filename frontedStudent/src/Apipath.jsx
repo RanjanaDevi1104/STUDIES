b677c97ae75f69e65d6cc0c6bd2e929325655fa8
RanjanaDevi1104 <ranju94644@gmail.com>
2026-02-27 20:45:40 +0530

    new message

--- frontedStudent/src/Apipath.jsx
+++ frontedStudent/src/Apipath.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 
-// export const BASE_URL = "https://studies-qv8r.onrender.com";
+export const BASE_URL = "https://studies-qv8r.onrender.com";
